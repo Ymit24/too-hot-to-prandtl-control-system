746ee179db548d316091449a72cac344c9426913
Christian <christianryansmith@gmail.com>
2024-05-02 20:49:27 -0500

    Added feedback controller with tests

--- control_system/src/controls.rs
+++ control_system/src/controls.rs
@@ -1,4 +1,4 @@
-use common::physical::{Percentage, ValveState};
+use common::physical::{Percentage, Rpm, ValveState};
 use once_cell::sync::Lazy;
 
 use crate::models::{
@@ -64,21 +64,16 @@ const VALVE_CURVE: Lazy<Curve<Temperature, ValveState>> = Lazy::new(|| {
     .expect("Failed to get valve curve.")
 });
 
+/// Closed loop feedback sensitivity K.
+/// Higher value means more sensitive;
+const PUMP_SENSITIVITY_K: f32 = 0.15f32;
+
 pub fn generate_control_frame(
     client_sensor_data: ClientSensorData,
     host_sensor_data: HostSensorData,
 ) -> ControlEvent {
     let temperature = host_sensor_data.cpu_temperature;
-    let target_pump_percent = match PUMP_CURVE.lookup(temperature) {
-        None => {
-            tracing::error!(
-                "Failed to get pump value for temperature {}. Defaulting to 100%!",
-                temperature
-            );
-            Percentage::try_from(100f32).expect("Failed to get percentage.")
-        }
-        Some(percentage) => percentage,
-    };
+    let target_pump_percent = pump_controller(temperature, client_sensor_data.pump_speed);
     let target_fan_percent = match FAN_CURVE.lookup(temperature) {
         None => {
             tracing::error!(
@@ -105,6 +100,37 @@ pub fn generate_control_frame(
         pump_activation: target_pump_percent,
         valve_state: target_valve_state,
     }
+}
+
+/// Apply the `Pump Controller` control system.
+fn pump_controller(temperature: Temperature, pump_rpm: Rpm) -> Percentage {
+    let target_activation = match PUMP_CURVE.lookup(temperature) {
+        None => {
+            tracing::error!(
+                "Failed to get pump value for temperature {}. Defaulting to 100%!",
+                temperature
+            );
+            Percentage::try_from(100f32).expect("Failed to get percentage.")
+        }
+        Some(percentage) => percentage,
+    };
+    let raw_current_speed_percentage: f32 = pump_rpm.into_percentage().into();
+    let raw_target: f32 = target_activation.into();
+
+    let raw_feedback_target = apply_feedback(raw_current_speed_percentage, raw_target);
+    match Percentage::try_from(raw_feedback_target) {
+        Err(err) => {
+            tracing::warn!("Failed to convert target activation percentage into `Percentage`. Clamping to min/max bounds. Error: {}", err);
+            Percentage::try_from(raw_current_speed_percentage.clamp(0f32, 100f32))
+                .expect("Failed to get Percentage.")
+        }
+        Ok(perc) => perc,
+    }
+}
+
+/// Apply basic feedback with `PUMP_SENSITIVITY_K` parameter.
+fn apply_feedback(current: f32, target: f32) -> f32 {
+    current + ((target - current) * PUMP_SENSITIVITY_K)
 }
 
 #[cfg(test)]
@@ -135,11 +161,15 @@ mod testing {
                     .lookup(host.cpu_temperature)
                     .expect("Failed to get curve value.")
             );
+            let raw_current_pump_speed = client.pump_speed.into_percentage().into();
+            let raw_target = PUMP_CURVE
+                .lookup(host.cpu_temperature)
+                .expect("Failed to get curve value.")
+                .into();
             assert_eq!(
                 control_frame.pump_activation,
-                PUMP_CURVE
-                    .lookup(host.cpu_temperature)
-                    .expect("Failed to get curve value.")
+                Percentage::try_from(apply_feedback(raw_current_pump_speed, raw_target))
+                    .expect("Failed to get Percentage.")
             );
             assert_eq!(
                 control_frame.valve_state,
@@ -147,6 +177,21 @@ mod testing {
                     .lookup(host.cpu_temperature)
                     .expect("Failed to get curve value.")
             );
+        }
+    }
+
+    #[test]
+    fn test_apply_feedback() {
+        for current in 0..100 {
+            for target in 0..100 {
+                let current = current as f32;
+                let target = target as f32;
+                let result = apply_feedback(current, target);
+
+                let correct = current + ((target - current) * PUMP_SENSITIVITY_K);
+
+                assert_eq!(result, correct);
+            }
         }
     }
 }
