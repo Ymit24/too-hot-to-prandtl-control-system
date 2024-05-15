5e98f525d5f6c5d8158d2e5b0744281ba8fb4cce
Christian Smith <christianryansmith@gmail.com>
2024-05-15 09:58:50 -0500

    Removed old models and refactored project structure

--- control_system/src/externals/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod event_logging;
-pub mod hardware;
-pub mod client_sensors;
-pub mod host_sensors;
-pub mod reporting_tool;
--- control_system/src/models/mod.rs
+++ control_system/src/models/mod.rs
@@ -1,7 +1,5 @@
 pub mod client_sensor_data;
 pub mod control_event;
-pub mod host_sensor_data;
-pub mod rpm;
-pub mod temperature;
-pub mod voltage;
 pub mod curve;
+pub mod host_sensor_data;
+pub mod temperature;
